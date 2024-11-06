a73f70021669ed3090bdc8e4e5a8dc64c7e17b2d
SocaciVictor <147531728+SocaciVictor@users.noreply.github.com>
2024-11-06 09:42:53 +0200

    Initialize2

---
+++ TravelApp/app/index.tsx
@@ -0,0 +1,16 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text>Open App.tsx to start working on your app!</Text>
+    </View>
+  );
+}
