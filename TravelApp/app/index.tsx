cc84f6f74d27ec57380e7eae4902ba12968a3a64
SocaciVictor <147531728+SocaciVictor@users.noreply.github.com>
2025-01-09 17:14:22 +0200

    Update to SDK 52

--- TravelApp/app/index.tsx
+++ TravelApp/app/index.tsx
@@ -10,7 +10,6 @@ export default function Index() {
       }}
     >
       <Text>Edit app/index.tsx to edit this screen.</Text>
-      <Text>Open App.tsx to start working on your app!</Text>
     </View>
   );
 }
